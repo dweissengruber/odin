10c163c5b9ed518943d6bef9924cde970c703680
Daniel Weissengruber <d.weissengruber@ixolit.com>
2021-03-05 14:25:09 +0100

    ✨  update scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,25 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\AllCheckersCommand::class,
+        Commands\BrowserConsoleCommand::class,
+        Commands\CertificateCheckCommand::class,
+        Commands\CrawlSiteCommand::class,
+        Commands\DnsCheckCommand::class,
+        Commands\OpenGraphCheckCommand::class,
+        Commands\ResetQueueStatus::class,
+        Commands\RobotCheckCommand::class,
+        Commands\ScanCertificateCommand::class,
+        Commands\ScanConsoleCommand::class,
+        Commands\ScanCrawlerCommand::class,
+        Commands\ScanDnsCommand::class,
+        Commands\ScanEverythingCommand::class,
+        Commands\ScanOpenGraphCommand::class,
+        Commands\ScanRobotsCommand::class,
+        Commands\ScanUptimeCommand::class,
+        Commands\ScanVisualDiffsCommand::class,
+        Commands\UptimeCheckCommand::class,
+        Commands\VisualDiffCommand::class
     ];
 
     /**
@@ -24,26 +42,14 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('scan:uptime')->everyFiveMinutes()->withoutOverlapping()->runInBackground();
+        $schedule->command('scan:uptime')->everyMinute()->withoutOverlapping()->runInBackground();
         $schedule->command('scan:robots')->hourly()->withoutOverlapping()->runInBackground();
         $schedule->command('scan:dns')->hourly()->withoutOverlapping()->runInBackground();
         $schedule->command('scan:certificates')->dailyAt('08:00:00')->withoutOverlapping()->runInBackground();
         $schedule->command('scan:opengraph')->dailyAt('08:00:00')->withoutOverlapping()->runInBackground();
-        $schedule->command('scan:consoles')->daily()->withoutOverlapping();
-        $schedule->command('scan:visual-diffs')->daily()->withoutOverlapping();
-        $schedule->command('scan:crawler')->weekly()->withoutOverlapping();
-        $schedule->command('horizon:snapshot')->everyFiveMinutes();
-    }
-
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
+        $schedule->command('scan:consoles')->daily()->withoutOverlapping()->runInBackground();
+        $schedule->command('scan:visual-diffs')->daily()->withoutOverlapping()->runInBackground();
+        $schedule->command('scan:crawler')->weekly()->withoutOverlapping()->runInBackground();
+        $schedule->command('horizon:snapshot')->everyFiveMinutes()->withoutOverlapping()->runInBackground();
     }
 }
